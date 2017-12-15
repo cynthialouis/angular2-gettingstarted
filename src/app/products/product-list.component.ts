c0742659822d17b9234c3dcc52d314e89e7597e9
Cynthia Louis <cynthialouis@MacBook-Pro-de-Cynthia.local>
2017-12-15 15:52:08 +0100

    Nested components (@Input @Output) + filtering a list

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,7 +12,8 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+  _listFilter: string;
+  filteredProducts: IProduct[];
 
   products: IProduct[] = [
     {
@@ -37,9 +38,38 @@ export class ProductListComponent implements OnInit {
     },
   ];
 
+  // Constructor
+
+
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
+
+// Getters and setters
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+
   // METHODS
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product list: ' + message;
   }
 
   // Interface methods implementation
